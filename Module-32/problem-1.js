b6d5de07d5fed989af40130ac88bec3c030fbe69
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-02-21 22:37:50 +0600

    filter method problem 2

--- Module-32/problem-1.js
+++ Module-32/problem-1.js
@@ -1,11 +1,17 @@
-
+// Problem: 1
 const odds =  [ 1, 3, 5, 7, 9 ];
 
 const evenForLoop = [];
 for(let odd of odds){
     evenForLoop.push(odd + 1);
 }
-console.log(evenForLoop);
+// console.log(evenForLoop);
 
 const evenMap = odds.map( odd => odd + 1);
-console.log(evenMap);
+// console.log(evenMap);
+
+//Problem: 2
+const arr = [33, 50, 79, 78, 90, 101, 30 ];
+
+const divisibleBy10 = arr.filter( num => num % 10 === 0);
+console.log(divisibleBy10);
