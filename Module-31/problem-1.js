d500d50f7f64412ac313ed2d486eb47a3ed3ec13
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-02-21 22:13:38 +0600

    Problem 1

--- Module-31/problem-1.js
+++ Module-31/problem-1.js
@@ -3,3 +3,12 @@ const mul = (a, b, c) => a * b * c;
 
 const result = mul(4, 5, 5);
 console.log(result);
+
+const sentence = `I am a web developer.
+I love to code. 
+I love to ear biryani.
+`
+console.log(sentence);
+
+const add = (a, b = 5) => a + b;
+console.log(add(4));
