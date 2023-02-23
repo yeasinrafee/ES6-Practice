ea9b7844bcd341ccf816abfc8a82df03d7e2abd6
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-02-23 15:00:43 +0600

    problem-6

---
+++ More-Practice/problem-6.js
@@ -0,0 +1,33 @@
+const studentData = [
+  {
+    class: 10,
+    details: [
+      {
+        studentId: "1",
+        gradingDetails: [{ grade: "A" }],
+      },
+      {
+        studentId: "2",
+        gradingDetails: [{ grade: "B" }],
+      },
+    ],
+  },
+  {
+    class: 11,
+    details: [
+      {
+        studentId: 3,
+        gradingDetails: [{ grade: "B" }],
+      },
+      {
+        studentId: 4,
+        gradingDetails: [{ grade: "D" }],
+      },
+    ],
+  },
+];
+
+const a = studentData[1].details[0].gradingDetails[0].grade;
+const b = studentData[1].details[1].gradingDetails[0].grade;
+console.log(a);
+console.log(b);
