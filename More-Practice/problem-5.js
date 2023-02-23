5791eeeb0741a5ffb319687ed31cfe85f7200923
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-02-23 14:57:27 +0600

    problem-5

---
+++ More-Practice/problem-5.js
@@ -0,0 +1,26 @@
+let instructor = {
+  name: "Jhankar Mahbub",
+  entrepreneur: true,
+  additionalData: {
+    writer: true,
+    favoriteHobbies: ["travelling", "Coding"],
+    books: ["programming er bolod to bos", "programming ercoddogosti"],
+    moreDetails: {
+      favoriteBasketballTeam: "XYZ",
+      isYoungest: true,
+      hometown: {
+        city: "ABC",
+        state: "VW",
+      },
+      countriesLivedIn: ["Bangladesh", "New York"],
+    },
+  },
+};
+
+const a = instructor.additionalData.books[1];
+const b = instructor.additionalData.moreDetails.hometown.state;
+const c = instructor.additionalData.moreDetails.countriesLivedIn[0];
+
+console.log(a);
+console.log(b);
+console.log(c);
