bb2e0fa0c2faff2bbc8b970c81d103c990d03504
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-02-23 14:27:09 +0600

    Usloved

---
+++ Module-32/access-object-value.js
@@ -0,0 +1,40 @@
+
+let data = {
+    location:[
+        {
+            latitude: '34.5 , 37.8',
+            longitude: '98.77 , 58.7',
+            city: 'Hyderabad',
+            country: 'India',
+        }
+    ]
+};
+console.log(data.location[0].city);
+
+const datas = {
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874",
+      "geo": {
+        "lat": "-37.3159",
+        "lng": "81.1496"
+      }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+      "name": "Romaguera-Crona",
+      "catchPhrase": "Multi-layered client-server neural-net",
+      "bs": "harness real-time e-markets"
+    }
+  };
+
+
+const dataObj = JSON.parse(datas);
+console.log(dataObj);
